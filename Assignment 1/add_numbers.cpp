95aecebe38fbf1e9805ca4a391e08ad175d691f8
keefanimentari <keefani05@gmail.com>
2024-09-01 21:53:16 +0700

    Update add_numbers.cpp

--- Assignment 1/add_numbers.cpp
+++ Assignment 1/add_numbers.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, sum;
+
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+
+    sum = a + b;
+
+    cout << "Sum: " << sum << std::endl;
+
+    return 0;
+}
 
