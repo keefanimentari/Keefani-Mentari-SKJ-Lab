33d831a9a3f8f7d8972f9ddbda396293c3a66909
keefanimentari <keefani05@gmail.com>
2024-09-02 14:48:21 +0700

    Create add_numbers.cpp

---
+++ Assignment 1/First Task/add_numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, sum;
+    a = 2;
+    b = 5;
+    sum = a + b;
+
+    cout << "Sum: " << sum << std::endl;
+
+    return 0;
+}
